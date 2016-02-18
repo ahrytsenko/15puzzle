bfd345172a20d111eca416070091aa17f7b106d5
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-02-18 14:18:32 +0200

    Update 15puzzle_core.py

--- src/15puzzle_core.py
+++ src/15puzzle_core.py
@@ -7,49 +7,68 @@ import simplegui
 
 lstPlaces = []
 lstMovableDraughts = []
-Name: iFreePlace = 0
-Type: integer
-Description: ID of a free place
+iFreePlace = 0
+iMovements = 0
+PUZZLE_SIZE = 4
+IDX_MIN = 0
+IDX_MAX = PUZZLE_SIZE-1
 
-Name: iMovements
-Type: integer
-Description: Movements were made since the start solving the current puzzle 
+#Name: isOrdered
+#In: lst
+#Out: boolean
+#Description: Check if all graughts are at the right places. 
+#Simply to check if ID of place is equal to ID of draught at that place (lstPlaces[i] == i)
+def isOrdered(aList):
+    bOrdered = True
+    for i in range(aList.len()):
+        if bOrdered:
+            bOrdered = (aList[i] == i)
+    return bOrdered
 
-Name: PUZZLE_SIZE
-Type: constant (integer)
-Description: Amount of rows (and colls cause its are equal). In classical game it is equal to 4
+#Name: isMovable
+#In: ID
+#Out: boolean
+#Description: Check if is it possible to move a draught from selected place to a free place. 
+#As we have a list of such places (lstMovableDraughts) the task is to check if selected ID is in the list.
+def isMovable(aList, ID):
+    bPossible = False
+    for placeID in aList:
+        if !bPossible:
+            bPossible = (placeID == ID)
+    return bPossible
 
-Name: IDX_MIN
-Type: constant (integer, usually equals to 0)
-Description: Minimal index of array
+#Name: moveDraught
+#In: lstPlaces, "ID of place"
+#Out: none
+#Description: Exchange IDs of draughts at places "ID of place" and iFreePlace (lstPlaces["ID of place"] <==> lstPlaces[iFreePlace].
+#After that change the ID of free place (iFreePlace = "ID of place") and fresh lstMovableDraughts.
+def moveDraught(aList, ID):
+    pass
 
-Name: IDX_MAX
-Type: constant (integer, compute from PUZZLE_SIZE, usually equals to PUZZLE_SIZE-1)
-Description: Maximal index of array
-/////////////////////////////
+def getID(aRow, aCol, aSize):
+    return (aRow*(aSize-1) + aCol)
+    
+def getRow(ID, aSize):
+    return (ID / aSize)
+    
+def getCol(ID, aSize):
+    return (ID % aSize)
+    
 
-Section 2. Core functions
-Name: isOrdered
-In: lst
-Out: boolean
-Description: Check if all graughts are at the right places. 
-Simply to check if ID of place is equal to ID of draught at that place (lstPlaces[i] == i)
 
-Name: isMovable
-In: ID
-Out: boolean
-Description: Check if is it possible to move a draught from selected place to a free place. 
-As we have a list of such places (lstMovableDraughts) the task is to check if selected ID is in the list.
+# Test area
+PUZZLE_SIZE = 4
+IDX_MIN = 0
+IDX_MAX = PUZZLE_SIZE-1
 
-Name: moveDraught
-In: lstPlaces, "ID of place"
-Out: none
-Description: Exchange IDs of draughts at places "ID of place" and iFreePlace (lstPlaces["ID of place"] <==> lstPlaces[iFreePlace].
-After that change the ID of free place (iFreePlace = "ID of place") and fresh lstMovableDraughts.
+for i in range(PUZZLE_SIZE*PUZZLE_SIZE):
+    lstPlaces.append(i)
 
-Name: 
-In: 
-Out: 
-Description: 
+print lstPlaces
+print isOrdered(lstPlaces)
+
+#lstMovableDraughts = []
+#iFreePlace = 0
+#iMovements = 0
 
 
