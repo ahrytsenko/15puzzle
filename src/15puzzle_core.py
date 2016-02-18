1adfe43fc30816f4e3bda115a0376c93cae8d82a
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-02-18 15:25:53 +0200

    Update 15puzzle_core.py

--- src/15puzzle_core.py
+++ src/15puzzle_core.py
@@ -5,54 +5,60 @@
 
 import simplegui
 
-lstPlaces = []
-lstMovableDraughts = []
-iFreePlace = 0
-iMovements = 0
 PUZZLE_SIZE = 4
 IDX_MIN = 0
 IDX_MAX = PUZZLE_SIZE-1
+
+class FPuzzle:
+    def __init__(self, puzzle_size = 4):
+        self.PUZZLE_SIZE = puzzle_size
+        self.lstPlaces = []
+        for i in range(self.PUZZLE_SIZE*self.PUZZLE_SIZE):
+            self.lstPlaces.append(i)
+        self.lstMovableDraughts = []
+        self.iFreePlace = 0
+        self.iMovements = 0
 
 #Name: isOrdered
 #In: lst
 #Out: boolean
 #Description: Check if all graughts are at the right places. 
 #Simply to check if ID of place is equal to ID of draught at that place (lstPlaces[i] == i)
-def isOrdered(aList):
-    bOrdered = True
-    for i in range(len(aList)):
-        if bOrdered:
-            bOrdered = (aList[i] == i)
-    return bOrdered
+    def isOrdered(self):
+        bOrdered = True
+        for i in range(len(self.lstPlaces)):
+            if bOrdered:
+                bOrdered = (self.lstPlaces[i] == i)
+        return bOrdered
 
 #Name: isMovable
 #In: ID
 #Out: boolean
 #Description: Check if is it possible to move a draught from selected place to a free place. 
 #As we have a list of such places (lstMovableDraughts) the task is to check if selected ID is in the list.
-def isMovable(aList, ID):
-    bPossible = False
-    for placeID in aList:
-        if not bPossible:
-            bPossible = (placeID == ID)
-    return bPossible
+    def isMovable(self, ID):
+        bPossible = False
+        for placeID in self.lstMovableDraughts:
+            if not bPossible:
+                bPossible = (placeID == ID)
+        return bPossible
 
 #Name: moveDraught
 #In: lstPlaces, "ID of place"
 #Out: none
 #Description: Exchange IDs of draughts at places "ID of place" and iFreePlace (lstPlaces["ID of place"] <==> lstPlaces[iFreePlace].
 #After that change the ID of free place (iFreePlace = "ID of place") and fresh lstMovableDraughts.
-def moveDraught(aList, ID):
-    pass
+    def moveDraught(self, ID):
+        pass
 
-def getID(aRow, aCol, aSize):
-    return (aRow*(aSize) + aCol)
-    
-def getRow(ID, aSize):
-    return (ID / aSize)
-    
-def getCol(ID, aSize):
-    return (ID % aSize)
+    def getID(self, aRow, aCol):
+        return (aRow*(self.PUZZLE_SIZE) + aCol)
+
+    def getRow(self, ID):
+        return (ID / self.PUZZLE_SIZE)
+
+    def getCol(self, ID):
+        return (ID % self.PUZZLE_SIZE)
     
 
 
@@ -61,27 +67,26 @@ PUZZLE_SIZE = 4
 IDX_MIN = 0
 IDX_MAX = PUZZLE_SIZE-1
 
-for i in range(PUZZLE_SIZE*PUZZLE_SIZE):
-    lstPlaces.append(i)
+fp = FPuzzle(4)
 
-print lstPlaces
-print isOrdered(lstPlaces)
+print fp.lstPlaces
+print fp.isOrdered()
 
 #lstMovableDraughts = []
-#iFreePlace = 0
+iFreePlace = 15
 #iMovements = 0
 
-print getID(0, 0, PUZZLE_SIZE)
-print getID(2, 2, PUZZLE_SIZE)
-print getID(3, 0, PUZZLE_SIZE)
+print fp.getID(0, 0)
+print fp.getID(2, 2)
+print fp.getID(3, 0)
 print ""
-print getRow(5, PUZZLE_SIZE)
-print getRow(1, PUZZLE_SIZE)
-print getRow(15, PUZZLE_SIZE)
+print fp.getRow(5)
+print fp.getRow(1)
+print fp.getRow(15)
 print ""
-print getCol(5, PUZZLE_SIZE)
-print getCol(1, PUZZLE_SIZE)
-print getCol(15, PUZZLE_SIZE)
+print fp.getCol(5)
+print fp.getCol(1)
+print fp.getCol(15)
 
 
 
