60313692c23f4a005517da38d4577ec8271e42c0
ahrytsenko <ahrytsenko@users.noreply.github.com>
2016-02-19 16:50:36 +0200

    Update 15puzzle_core.py

--- src/15puzzle_core.py
+++ src/15puzzle_core.py
@@ -7,56 +7,21 @@ import simplegui
 import random
 
 class FifteenPuzzleCore:
+    
     def __init__(self, puzzle_size = 4, free_place = 15):
         self.PUZZLE_SIZE = puzzle_size
         self.FREE_PLACE = free_place
         self.lstPlaces = []
-        for i in range(self.PUZZLE_SIZE*self.PUZZLE_SIZE):
-            self.lstPlaces.append(i)
+        self.lstPlaces = range(self.PUZZLE_SIZE*self.PUZZLE_SIZE)
         self.lstMovableDraughts = []
         self.iFreePlace = 15
         self.iMovements = 0
         self.updateMovableDraughts()
         
+    #Private methods
     def __str__(self):
         pass
         #return "Draughts: " + self.lstPlaces + 
-
-    #Name: isOrdered
-    #In: lst
-    #Out: boolean
-    #Description: Check if all graughts are at the right places. 
-    #Simply to check if ID of place is equal to ID of draught at that place (lstPlaces[i] == i)
-    def isOrdered(self):
-        bOrdered = True
-        for i in range(len(self.lstPlaces)):
-            if bOrdered:
-                bOrdered = (self.lstPlaces[i] == i)
-        return bOrdered
-
-    #Name: isMovable
-    #In: ID
-    #Out: boolean
-    #Description: Check if is it possible to move a draught from selected place to a free place. 
-    #As we have a list of such places (lstMovableDraughts) the task is to check if selected ID is in the list.
-    def isMovable(self, ID):
-        bPossible = False
-        for placeID in self.lstMovableDraughts:
-            if not bPossible:
-                bPossible = (placeID == ID)
-        return bPossible
-
-    #Name: moveDraught
-    #In: lstPlaces, "ID of place"
-    #Out: none
-    #Description: Exchange IDs of draughts at places "ID of place" and iFreePlace (lstPlaces["ID of place"] <==> lstPlaces[iFreePlace].
-    #After that change the ID of free place (iFreePlace = "ID of place") and fresh lstMovableDraughts.
-    def moveDraught(self, ID):
-        self.lstPlaces[self.iFreePlace] = self.lstPlaces[ID]
-        self.lstPlaces[ID] = self.FREE_PLACE
-        self.iFreePlace = ID
-        self.updateMovableDraughts()
-        self.iMovements += 1
 
     def getID(self, aRow, aCol):
         return (aRow*(self.PUZZLE_SIZE) + aCol)
@@ -78,6 +43,35 @@ class FifteenPuzzleCore:
         if self.getCol(self.iFreePlace) < self.PUZZLE_SIZE-1:
             self.lstMovableDraughts.append(self.getID(self.getRow(self.iFreePlace), self.getCol(self.iFreePlace)+1))
     
+    #Public methods
+    
+    #Description: Check if all graughts are at the right places. 
+    #Simply to check if ID of place is equal to ID of draught at that place (lstPlaces[i] == i)
+    def isOrdered(self):
+        bOrdered = True
+        for i in range(len(self.lstPlaces)):
+            if bOrdered:
+                bOrdered = (self.lstPlaces[i] == i)
+        return bOrdered
+
+    #Description: Check if is it possible to move a draught from selected place to a free place. 
+    #As we have a list of such places (lstMovableDraughts) the task is to check if selected ID is in the list.
+    def isMovable(self, ID):
+        bPossible = False
+        for placeID in self.lstMovableDraughts:
+            if not bPossible:
+                bPossible = (placeID == ID)
+        return bPossible
+
+    #Description: Exchange IDs of draughts at places "ID of place" and iFreePlace (lstPlaces["ID of place"] <==> lstPlaces[iFreePlace].
+    #After that change the ID of free place (iFreePlace = "ID of place") and fresh lstMovableDraughts.
+    def moveDraught(self, ID):
+        self.lstPlaces[self.iFreePlace] = self.lstPlaces[ID]
+        self.lstPlaces[ID] = self.FREE_PLACE
+        self.iFreePlace = ID
+        self.updateMovableDraughts()
+        self.iMovements += 1
+
     def shuffle(self):
         random.shuffle(self.lstPlaces)
         self.iMovements = 0
